21fe8d220ba73724c7fa39acf3828d4b240fdf99
ottfoekst <ottfoekst@gmail.com>
2022-03-25 01:20:42 +0900

    トップ画面の右上に追加ボタンを設置

--- app/src/main/java/com/ottfoekst/instanceplaybacker/MainActivity.java
+++ app/src/main/java/com/ottfoekst/instanceplaybacker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ottfoekst.instanceplaybacker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
     }
 }
